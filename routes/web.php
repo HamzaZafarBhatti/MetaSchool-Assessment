53c7405b8f68f426050ba83a8c8c5d5543e6d2e0
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-02-21 11:14:48 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    if (!auth()->user()) {
-        return view('auth.login');
-    } else {
-        return redirect('dashboard');
-    }
-});
+Route::redirect('/', 'login');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::middleware(['auth:sanctum', 'verified'])->resource('todos', TodoController::class);
